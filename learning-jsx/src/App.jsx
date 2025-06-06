1c596d2829091d1b801206737f823cd6ffb8fa3a
yeono <126737362+KYDDD@users.noreply.github.com>
2025-06-06 14:41:36 +0900

    initial commit

---
+++ learning-jsx/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ButtonA from "./ButtonA";
+import ButtonB from "./ButtonB";
+
+function App() {
+  return (
+    <>
+      <ButtonA />
+      <ButtonB />
+    </>
+  );
+}
+
+export default App;
---
+++ learning-jsx/src/ButtonA.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ButtonA.module.css";
+
+function ButtonA() {
+  return <button className={styles.button}>Button A</button>;
+}
+
+export default ButtonA;
