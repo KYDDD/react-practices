93cefcfa4ebe78d11d50c314a422d81c89d35952
yeono <126737362+KYDDD@users.noreply.github.com>
2025-06-15 01:56:44 +0900

    useEffect

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { useState } from "react";
+import Timer from "./Timer";
+
+const App = () => {
+  const [showTimer, setShowTimer] = useState(false);
+
+  return (
+    <>
+      <label>
+        <input type="checkbox" checked={showTimer} onChange={(e) => setShowTimer(e.target.checked)} />
+        Show Timer
+      </label>
+      {showTimer && <Timer />}
+    </>
+  );
+};
+
+export default App;
