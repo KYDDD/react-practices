8d80fc8d4fa3d76c34eb754cc769ed8528d59ae3
yeono <126737362+KYDDD@users.noreply.github.com>
2025-06-15 15:03:33 +0900

    useEffect-star

---
+++ useEffect-star/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState("");
+
+  const handleCountUpdate = () => {
+    setCount(count + 1);
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  // 마운트  + [item] 변경될때만 실행
+  useEffect(() => {
+    console.log("count 변화");
+  }, [count]);
+
+  return (
+    <>
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count: {count}</span>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span>name: {name}</span>
+    </>
+  );
+}
+
+export default App;
