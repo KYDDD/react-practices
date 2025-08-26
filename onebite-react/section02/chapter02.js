4ff2e1202651df703f1f05bb859af0ad7011bb9a
yeono <126737362+KYDDD@users.noreply.github.com>
2025-08-26 15:39:38 +0900

    2-2

---
+++ onebite-react/section02/chapter02.js
@@ -0,0 +1,16 @@
+// function returnFalse() {
+//   console.log("False함수");
+//   return false;
+// }
+
+// function returnTrue() {
+//   console.log("True함수");
+//   return true;
+// }
+
+// console.log(returnFalse() && returnTrue());
+
+// 단락 평가 활용 사례
+function printName(person) {
+  console.log(person && person.name);
+}
