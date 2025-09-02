e0b8d4c56aca7884a6ab1579b5536ef7ccc3f933
김연호 <kim-yeonho@Big-Mac.local>
2025-09-02 14:58:26 +0900

    section03

---
+++ onebite-react/section03/src/index.js
@@ -0,0 +1,10 @@
+// const { add, sub } = require("./math");
+// import multiply, { add, sub } from "./math.js";
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multiply(2, 3));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
