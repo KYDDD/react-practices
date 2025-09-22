6b1ed9f3da64d41711544ef70406af78192ae760
김연호 <riderkim88@naver.com>
2025-09-22 22:14:05 +0900

    section12.12

--- onebite-react/section12re/src/pages/Edit.jsx
+++ onebite-react/section12re/src/pages/Edit.jsx
@@ -2,5 +2,5 @@ import { useParams } from "react-router-dom";
 
 export default function Edit() {
   const params = useParams();
-  return <div>{params.id}번 일기입니다.</div>;
+  return <div>{params.id}번 일기수정 페이지 입니다.</div>;
 }
--- onebite-react/section12re/src/pages/Home.jsx
+++ onebite-react/section12re/src/pages/Home.jsx
@@ -1,3 +1,39 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+
+// 함수가 매개변수만으로도 필요한 데이터를 다 제공받을수 있으면 컴포넌트 외부에 선언.
+const getMonthlyData = (pivotDate, data) => {
+  const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime();
+
+  const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1, 0, 23, 59, 59).getTime();
+
+  return data.filter((item) => {
+    return item.createdDate >= beginTime && item.createdDate <= endTime;
+  });
+};
 export default function Home() {
-  return <div>Home</div>;
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+      />
+      <DiaryList data={monthlyData} />
+    </div>
+  );
 }
