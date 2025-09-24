f40ed7f4c353042c33d7a84cb1d3f4e39aa776b2
김연호 <riderkim88@naver.com>
2025-09-24 14:51:05 +0900

    section 12.14

--- onebite-react/section12re/src/pages/New.jsx
+++ onebite-react/section12re/src/pages/New.jsx
@@ -1,3 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import Header from "../components/Header";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
 export default function New() {
-  return <div>New</div>;
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+
+    //replace옵션으로 뒤로가기 방지
+    nav("/", { replace: true });
+  };
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={
+          <Button
+            onClick={() => {
+              nav(-1);
+            }}
+            text={"< 뒤로 가기"}
+          />
+        }
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 }
