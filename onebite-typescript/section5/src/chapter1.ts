f3bb8000e69a72aa5d7edd126886f28c73b2d828
김연호 <riderkim88@naver.com>
2025-10-16 12:54:13 +0900

    section5

---
+++ onebite-typescript/section5/src/chapter1.ts
@@ -0,0 +1,33 @@
+// 인터페이스 확장
+
+type Animal = {
+  name: string;
+  color: string;
+};
+
+interface Dog extends Animal {
+  isBark: boolean;
+}
+
+const dog: Dog = {
+  name: "hello",
+  color: "",
+  isBark: true,
+};
+
+interface Cat extends Animal {
+  isScratch: boolean;
+}
+
+interface Chick extends Animal {
+  isFly: boolean;
+}
+
+interface DogCat extends Dog, Cat {}
+
+const dogCat: DogCat = {
+  name: "",
+  color: "",
+  isBark: true,
+  isScratch: true,
+};
