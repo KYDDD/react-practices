36339061afb8527232269963ba286594c981a11a
김연호 <riderkim88@naver.com>
2025-10-23 15:17:17 +0900

    section8

---
+++ onebite-typescript/section8/src/chapter2.ts
@@ -0,0 +1,14 @@
+//keyof 연산자
+
+type Person = typeof person;
+
+function getPropertyKey(person: Person, key: keyof typeof person) {
+  return person[key];
+}
+
+const person = {
+  name: "김연호",
+  age: 27,
+};
+
+getPropertyKey(person, "name");
