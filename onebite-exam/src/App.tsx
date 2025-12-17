bfbad1eda1e4dbbf55a5082fc4301c0c436aedcd
김연호 <riderkim88@naver.com>
2025-12-17 23:06:42 +0900

    2.2

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      {/* 1. 타이포그래프 */}
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
+      <div className="text-lg font-bold">text-lg</div>
+      <div className="text-xl font-extrabold">text-xl</div>
+      <div className="text-2xl font-black">text-2xl</div>
+      <div className="text-[13px]">text-13px</div>
+      {/* 2. 백그라운드 컬러 */}
+      <div className="bg-amber-500">amber-500</div>
+
+      {/* 3. 사이즈 */}
+      <div className="h-20 w-full bg-blue-500">box</div>
+
+      {/* 4. 여백 */}
+      <div className="m-5 h-50 w-50 bg-red-400 px-5 py-5">
+        <div className="h-full w-full bg-blue-400"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
