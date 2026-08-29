58fc1fed185dd85503927f7603cca521ae6dd29f
김연호 <riderkim88@naver.com>
2026-08-29 23:14:19 +0900

    section06

---
+++ onebite-react-v2/section06/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function onClickButton(value) {
+    setCount(count + value);
+  }
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count}></Viewer>
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton}></Controller>
+      </section>
+    </div>
+  );
+}
+
+export default App;
