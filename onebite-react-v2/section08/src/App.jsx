89e7dca44d97d22db83df8f946a6296d287711df
김연호 <riderkim88@naver.com>
2026-09-06 17:11:55 +0900

    section09

--- onebite-react-v2/section08/src/App.jsx
+++ onebite-react-v2/section08/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 
 const mockData = [
   {
@@ -24,43 +24,48 @@ const mockData = [
     date: new Date().getTime(),
   },
 ];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) => (item.id === action.targetId ? { ...item, isDone: !item.isDone } : item));
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   function onCreate(content) {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   }
 
   function onUpdate(targetId) {
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === targetId) {
-          return {
-            ...todo,
-            isDone: !todo.isDone,
-          };
-        }
-        return todo;
-      }),
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   }
 
   function onDelete(targetId) {
-    //인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(
-      todos.filter((todo) => {
-        return todo.id !== targetId;
-      }),
-    );
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   }
   return (
     <div className="App">
